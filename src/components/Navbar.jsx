bca74327728ad60f8b6299c840b0e75b4b4e63a9
Hakim-56 <hakimhazlif@gmail.com>
2025-04-03 04:26:26 +0000

    feat(routing): configure dashboard navigation routes from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,56 +3,61 @@ import { Navigation } from './Navigation'
 import { RiBarChartFill } from 'react-icons/ri'
 import { TbBarbellFilled } from 'react-icons/tb'
 import { FaAppleAlt } from 'react-icons/fa'
-
-const navigations = [
-  {
-    link: '/',
-    children: (
-      <>
-        <IoHomeSharp size={18} />
-        <span>Home</span>
-      </>
-    ),
-  },
-  {
-    link: '#',
-    children: (
-      <>
-        <TbBarbellFilled size={18} />
-        <span>Workouts</span>
-      </>
-    ),
-  },
-  {
-    link: '#',
-    children: (
-      <>
-        <FaAppleAlt size={18} />
-        <span>Nutrition</span>
-      </>
-    ),
-  },
-  {
-    link: '#',
-    children: (
-      <>
-        <RiBarChartFill size={18} />
-        <span>Progress</span>
-      </>
-    ),
-  },
-  {
-    link: '#',
-    children: (
-      <>
-        <IoSettingsSharp size={18} />
-        <span>Settings</span>
-      </>
-    ),
-  },
-]
+import { useSelector } from 'react-redux'
+import { user } from '../app/slices/authSlice'
 
 const Navbar = ({ forDesktop = true, onClose = null }) => {
+  const { username } = useSelector(user)
+  const modifiedUsername = username.replace(' ', '-')
+
+  const navigations = [
+    {
+      link: '/',
+      children: (
+        <>
+          <IoHomeSharp size={18} />
+          <span>Home</span>
+        </>
+      ),
+    },
+    {
+      link: `/user/${modifiedUsername}/workouts`,
+      children: (
+        <>
+          <TbBarbellFilled size={18} />
+          <span>Workouts</span>
+        </>
+      ),
+    },
+    {
+      link: `/user/${modifiedUsername}/nutrition`,
+      children: (
+        <>
+          <FaAppleAlt size={18} />
+          <span>Nutrition</span>
+        </>
+      ),
+    },
+    {
+      link: `/user/${modifiedUsername}/progress`,
+      children: (
+        <>
+          <RiBarChartFill size={18} />
+          <span>Progress</span>
+        </>
+      ),
+    },
+    {
+      link: `/user/${modifiedUsername}/settings`,
+      children: (
+        <>
+          <IoSettingsSharp size={18} />
+          <span>Settings</span>
+        </>
+      ),
+    },
+  ]
+
   const navigationsMap = forDesktop ? navigations.slice(0, 3) : navigations
 
   return (
